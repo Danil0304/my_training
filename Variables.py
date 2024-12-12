6f13828607a36d9865be36c59f11f2e722a70ebd
Danil0304 <kapustindanil70@gmail.com>
2024-12-12 16:11:54 +0300

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,6 @@
+The_number_of_completed_DZ = 12
+The_number_of_hours_spent = 1.5
+Course_name = 'Python'
+total_tasks = 0.125
+print(1.5/12)
+print(Course_name,The_number_of_completed_DZ,The_number_of_hours_spent,total_tasks)
